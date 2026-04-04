d2f2378950e52709e1774428c045e706417490d9
iniyan <viniyan563@gmail.com>
2026-04-04 08:46:43 +0530

    updates

--- app/page.tsx
+++ app/page.tsx
@@ -20,22 +20,23 @@ export default function Home() {
     );
   }, []);
   return (
-    <div className=" bg-[#0c0c0b] text-white px-6 md:px-16 py-10 flex items-center justify-center">
-      <div className="grid md:grid-cols-2 items-center justify-items-center gap-5">
-        <div className="flex flex-col">
-          <div className="flex items-center  gap-5">
-            <p className="text-9xl font-pixel-grid">Iniyan V</p>
-            <div className="cursor w-10 h-25 bg-secondary -mt-5"></div>
+    <div className=" bg-[#0c0c0b] text-white px-6 md:px-16 py-10 min-h-screen flex items-center justify-center">
+      <div className="w-full max-w-7xl mx-auto">
+        <div className="grid md:grid-cols-2 items-center justify-items-center gap-5">
+          <div className="flex flex-col">
+            <div className="flex items-center  gap-5">
+              <p className="text-9xl font-pixel-grid">Iniyan</p>
+              <div className="cursor w-10 h-25 bg-secondary -mt-5"></div>
+            </div>
+            <p className="text-secondary text-2xl font-bold font-pixel-line">
+              ./ FULL STACK DEVELOPER : )
+            </p>
+            <p className="text-gray-200 pt-10 text-xl">
+              I build systems software, AI applications, and experimental tools.
+            </p>
+            <RetroButton Label="VIEW PROJECTS" />
           </div>
-          <p className="text-secondary text-2xl font-bold font-pixel-line">
-            ./ FULL STACK DEVELOPER : )
-          </p>
-          <p className="text-gray-200 pt-10 text-xl">
-            I build systems software, AI applications, and experimental tools.
-          </p>
-          <RetroButton Label="VIEW PROJECTS" />
         </div>
-        <Image src="/me.png" width={400} height={400} priority alt="profile" />
       </div>
     </div>
   );
