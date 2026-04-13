feea951590027c212be9e220aca2d0f123d74538
iniyan <viniyan563@gmail.com>
2026-04-13 11:17:19 +0530

    added props to the project component

--- app/components/project.tsx
+++ app/components/project.tsx
@@ -1,10 +1,25 @@
 import Image from "next/image";
-export default function Project() {
+
+interface ProjectProps {
+  title: string;
+  subtitle: string;
+  description: React.ReactNode;
+  imageSrc: string;
+  tags: string[];
+}
+
+export default function Project({
+  title,
+  subtitle,
+  description,
+  imageSrc,
+  tags,
+}: ProjectProps) {
   return (
     <div className="panel relative w-screen">
       <Image
-        src="/melouz.png"
-        alt="demo"
+        src={imageSrc}
+        alt={title}
         fill
         sizes="100vw"
         className="object-cover grayscale-20"
@@ -12,31 +27,20 @@ export default function Project() {
       <div className="absolute text-white top-110 inset-0 bg-linear-to-t from-black/90 to-transparent">
         <div className="flex flex-col">
           <p className="text-[25vh] pl-15 font-extrabold uppercase leading-none">
-            Project
+            {title}
           </p>
           <p className="text-2xl pt-10 pl-20 font-bold uppercase text-secondary">
-            World saving project
+            {subtitle}
           </p>
-          <p className="text-sm pl-20 text-gray-400">
-            This is a project written to save the world <br /> from ending in
-            just 5 days.
-          </p>
+          <p className="text-sm pl-20 text-gray-400">{description}</p>
           <div className="flex pl-20 pt-7 flex-row gap-5">
-            <div className="bg-white">
-              <p className="text-black font-mono uppercase font-bold p-1 text-sm">
-                NextJS
-              </p>
-            </div>
-            <div className="bg-white">
-              <p className="text-black font-mono uppercase font-bold p-1 text-sm">
-                NextJS
-              </p>
-            </div>
-            <div className="bg-white">
-              <p className="text-black font-mono uppercase font-bold p-1 text-sm">
-                NextJS
-              </p>
-            </div>
+            {tags.map((tag, index) => (
+              <div key={index} className="bg-white">
+                <p className="text-black font-mono uppercase font-bold p-1 text-sm">
+                  {tag}
+                </p>
+              </div>
+            ))}
           </div>
         </div>
       </div>
